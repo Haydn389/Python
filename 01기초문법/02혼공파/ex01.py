2acadd5df23f16c328b905fc51f3b00c23dd387c
Haydn389 <ekchoi0502@gmail.com>
2022-05-25 22:52:39 +0900

    0525 at Home

--- 01기초문법/02혼공파/ex01.py
+++ 01기초문법/02혼공파/ex01.py
@@ -1,17 +1,40 @@
 #################
 #p.53 여러줄 문자 """
 #p.53 여러줄 문자 사용시 줄바꿈 제거 \
-'''
-print("""
-안녕하세요
-안녕하세요
-안녕하세요
-""")
-'''
+# print("""
+# 안녕하세요
+# 안녕하세요
+# 안녕하세요
+# """)
+
 
 #################
-from platform import python_branch
+# from platform import python_branch
+
+# a=input("입력 : ")
+# print(a,type(a))
 
 
-a=input("임력 : 3")
-print(a,type(a))
+#p.95
+# print("{}".format(2,3))
+
+# print("{:+6d}".format(123))
+# print("{:+6d}".format(-123))
+# print("{:-6d}".format(123))
+# print("{:-6d}".format(-123))
+# print("{: 6d}".format(123))
+# print("{: 6d}".format(-123))
+
+# print("{:f}".format(12345))
+# print("{:f}".format(1234.5))
+# print("{:f}".format(123.45))
+# print("{:f}".format(12.345))
+# print("{:f}".format(1.2345))
+# print("{:f}".format(0.12345))
+# print("{:15f}".format(12.345))
+
+# print("{:10g}".format(52.000))
+
+#p.99 대소문자 바꾸기
+
+print("10 ,20 ,30 ,40,50".split(" ,"))
--- 01기초문법/02혼공파/test.py
+++ 01기초문법/02혼공파/test.py
@@ -1,3 +1,4 @@
-dic = { "1":[1,2,3,5]}
+# dic = { "1":[1,2,3,5]}
 
-print(dic["1"])
+# print(dic["1"])
+print("dasdfsd{}{}".format(1,"문자열"))
