7d6e23a428917e65b6f2c4832ea7d42af62e80d5
Haydn389 <ekchoi0502@gmail.com>
2022-05-28 17:59:02 +0900

    0528 in cafe

--- 01기초문법/02혼공파/ex02.py
+++ 01기초문법/02혼공파/ex02.py
@@ -2,7 +2,7 @@ import datetime
 
 n = datetime.datetime.now()
 
-# print("{}년 {}월 {}일 {}시 {}분 {}초입니다.".format(n.year,n.month,n.day,n.hour,n.minute,n.second))
+print("{}년 {}월 {}일 {}시 {}분 {}초입니다.".format(n.year,n.month,n.day,n.hour,n.minute,n.second))
 
 if n.hour < 12:
     print("현재시각은 {}시로 오전입니다.".format(n.hour))
