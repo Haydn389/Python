34418b2bcacf785c2f22830691eaf533ede7300e
Haydn389 <ekchoi0502@gmail.com>
2022-06-11 01:31:25 +0900

    0611

--- 03온라인저지/00코드업/기초100제.py
+++ 03온라인저지/00코드업/기초100제.py
@@ -1,4 +1,10 @@
-y, m, d=input().split('.')
+# y, m, d=input().split('.')
 
-print(d,m,y,sep="-")
+# print(d,m,y,sep="-")
 
+# print("\"C:\Download\\'hello'.py\"")
+# print("print("+'"Hello\\'+"n"+'World")')
+# print('print("Hello\\nWorld")')
+
+a,b = input() 
+print(a,b)
